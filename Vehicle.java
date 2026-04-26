b92098bb8945be84cb33e269941fa1f71799e3df
Imran Hossen <it24063@mbstu.ac.bd>
2026-04-26 10:34:16 +0600

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -6,6 +6,8 @@ public class Vehicle { // Create a class
                 this.color = color;
         }
         void start() { // Create a public method
+                system.out.print("Vehicle Brand: "+ brand);
+                 system.out.print("Vehicle Color: "+ color);
                 System.out.println("Vehicle is starting...");
         }
 }
